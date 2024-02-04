0716184de035a7e581d2a02fdf877ed898badc48
KolyanGrom <kolyangrom@yandex.ru>
2024-02-04 20:51:12 +0700

    Изменение названий в знаениях Пополнения и Бонусов

--- src/BonusMilesService.java
+++ src/BonusMilesService.java
@@ -2,9 +2,9 @@ public class BonusMilesService {
 
     public int calculate(int price) {
         int result;
-        int stepplus = 20;
-        int bonuse = 1;
-        result = (price / stepplus * bonuse);;
+        int stepPlus = 20;
+        int bonus = 1;
+        result = (price / stepPlus * bonus);;
         return result;
     }
     }
